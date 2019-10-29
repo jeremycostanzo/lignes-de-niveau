962238ad931e4c34579474b0c37914dde33213b8
Jeremy Costanzo <jeremy.costanzo@mines-paristech.fr>
2019-10-29 18:19:46 +0100

    Correction des fonctions permettant le calcul d'angles

--- Convert_rad.py
+++ Convert_rad.py
@@ -8,25 +8,19 @@ def rad(p):
     """
     Cette fonction calcule l'angle oriente (pt(1,0),p)
     """
-    x = p.x
-    y = p.y
-    if abs(x)>abs(y):
-        if y >= 0:
-            return(np.arccos(x))
-        else:
-            return(-np.arccos(x))
+    pn = p.normalized()
+    x = pn.x
+    y = pn.y
+    if y > 0:
+        return np.arccos(x)
     else:
-        if x >= 0:
-            return(np.arcsin(y))
-        else:
-            return(np.pi-np.arcsin(y))
+        return -np.arccos(x)
     
 def theta(p0,p):
     """
     calcule l'angle oriente (p0+pt(delta,0),p) ou delta = dist(p0,p)
     """
-    de = dist(p0,p)
-    return(rad((1/de)*(p-p0)))
+    return rad(p) - rad(p0)
 
 def p_a(p0,th,delta):
     """
@@ -34,3 +28,34 @@ def p_a(p0,th,delta):
     """
     p = delta*p_a1(th)
     return(p0+p)
+
+def newton(f,p0,c,eps=10**-4):
+    def fun(x,y):
+        return (f(x,y)-c)
+    def proch(p): #proch joue le role de l'application contractante pour l'application du theoreme du point fixe
+        g = grad(f,p)
+        return(p - (fun(p.x,p.y)/(g.norme()**2))*g)
+    p1 = proch(p0)
+    while dist(p0,p1) > eps:
+        p0 = p1
+        p1 = proch(p1)
+    return(p1)
+
+def g(f,p0,theta,d) :
+    return f(p0.x+d*np.cos(theta),p0.y+d*np.sin(theta))
+
+def dichotoproch(f,c,delta,ad,d):
+    ref = (d - ad) + d
+    ang = theta(d + pt(delta,0),ref)
+    def rech(th):
+        return g(f,d,th-ang,delta)
+    a = -np.pi + delta
+    b =  np.pi - delta
+    while b-a > delta:
+        m = (a+b)/2
+        if (rech(m)-c)*(rech(a)-c)<=0:
+            b = m
+        else:
+            a = m
+    t = (a+b)/2
+    return p_a(d,t+ang,delta)
